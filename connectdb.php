5727423818eb9fd9a22aafe587162ebc8d6d1323
Singhtaravirak Chea <singhtaravirak@gmail.com>
2019-09-16 21:30:18 +0700

    First commit

---
+++ connectdb.php
@@ -0,0 +1,13 @@
+<?php
+
+try{
+$pdo = new PDO('mysql:host=localhost;dbname=pos_db', 'root', ''); 
+
+echo "Connection Successful"; 
+    
+} catch(PDOException $f) {
+    echo $f->getmessage(); 
+}
+
+
+?>
