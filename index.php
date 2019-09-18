01ebbddc806716e6ce06029993c98c9d61347b9f
Singhtaravirak Chea <singhtaravirak@gmail.com>
2019-09-18 19:55:52 +0700

    Add sweetaleart.js to show successful log in message

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<!-- jQuery 3 -->
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="plugins/iCheck/icheck.min.js"></script>
+<script src="bower_components/sweetaleart/sweetaleart.js"></script>
 <?php
 include_once'connectdb.php'; 
 session_start(); 
@@ -18,20 +25,44 @@ if(isset($_POST['btn_login'])) {
         $_SESSION['username'] = $row['username'];
         $_SESSION['useremail'] = $row['useremail'];
         $_SESSION['role'] = $row['role'];
-//        print_r($_SESSION); 
-        echo $success = 'Login Successfully';
-        
+
+        echo '
+        <script type="text/javascript">
+            jQuery(function validation() {
+                swal({
+                  title: "Good job! '.$_SESSION['username'].'",
+                  text: "Details Matched",
+                  icon: "success",
+                  button: "Loading...",
+                });
+            });
+        </script>
+        ';
+    
         header('refresh:1;dashboard.php'); 
+        
     }else if($row['useremail']==$useremail AND $row['password']==$password AND $row['role']=="User") {
         
         $_SESSION['userid'] = $row['userid'];
         $_SESSION['username'] = $row['username'];
         $_SESSION['useremail'] = $row['useremail'];
         $_SESSION['role'] = $row['role'];
+        
+        echo '
+        <script type="text/javascript">
+            jQuery(function validation() {
+                swal({
+                  title: "Good job! '.$_SESSION['username'].'",
+                  text: "Details Matched",
+                  icon: "success",
+                  button: "Loading...",
+                });
+            });
+        </script>
+        ';
+        
         header('refresh:1;user.php');
-    } else {
-        echo 'Login Fail';
-    }
+    } 
     
     
     
@@ -80,11 +111,11 @@ if(isset($_POST['btn_login'])) {
 
     <form action="" method="post">
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email" name="txt_useremail">
+        <input type="email" class="form-control" placeholder="Email" name="txt_useremail" required>
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password" name="txt_password">
+        <input type="password" class="form-control" placeholder="Password" name="txt_password" required>
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
@@ -106,12 +137,8 @@ if(isset($_POST['btn_login'])) {
 </div>
 <!-- /.login-box -->
 
-<!-- jQuery 3 -->
-<script src="bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- iCheck -->
-<script src="plugins/iCheck/icheck.min.js"></script>
+
+
 <script>
   $(function () {
     $('input').iCheck({
