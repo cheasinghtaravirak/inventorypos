e74409f8fafd73d55d7ec8a66d9468e5883ae2b7
Singhtaravirak Chea <singhtaravirak@gmail.com>
2019-10-07 08:01:53 +0700

    Implement update functionality with new image upload and make form fields stay up-to-dated

--- editproduct.php
+++ editproduct.php
@@ -33,6 +33,86 @@ if(isset($_POST['btnupdate'])) {
     $f_name= $_FILES['myfile']['name'];
     if(!empty($f_name)) {
         
+        $f_tmp = $_FILES['myfile']['tmp_name'];
+        $f_size =  $_FILES['myfile']['size'];
+        $f_extension = explode('.',$f_name);
+        $f_extension= strtolower(end($f_extension));
+        $f_newfile =  uniqid().'.'. $f_extension;   
+        $store = "productimages/".$f_newfile;
+
+        //check for file extension 
+        if($f_extension=='jpg' || $f_extension=='jpeg' || $f_extension=='png' || $f_extension=='gif'){
+            if($f_size>=1000000 ){
+                $error =  '
+                <script type="text/javascript">
+                    jQuery(function validation() {
+                        swal({
+                          title: "Error!",
+                          text: "Max file should be 1MB",
+                          icon: "warning",
+                          button: "Ok",
+                        });
+                    });
+                </script>'; 
+                echo $error; 
+
+            }else{
+               if(move_uploaded_file($f_tmp,$store)){
+                   //new file to store in the database (its name is id number)
+//                   $productimage = $f_newfile; 
+                    if(!isset($error)) {
+                        $update = $pdo->prepare("update tbl_product set pname=:pname, pcategory=:pcategory, purchaseprice=:purchaseprice, saleprice=:saleprice, pstock=:pstock, pdescription=:pdescription, pimage=:pimage where pid = $id"); 
+                        $update->bindParam(':pname', $productname_txt); 
+                        $update->bindParam(':pcategory', $category_txt); 
+                        $update->bindParam(':purchaseprice', $purchaseprice_txt); 
+                        $update->bindParam(':saleprice', $saleprice_txt); 
+                        $update->bindParam(':pstock', $stock_txt); 
+                        $update->bindParam(':pdescription', $description_txt); 
+                        $update->bindParam(':pimage', $f_newfile);
+
+                        if($update->execute()) {
+                            echo '
+                            <script type="text/javascript">
+                                jQuery(function validation() {
+                                    swal({
+                                      title: "Updated!!!",
+                                      text: "The product is updated successfully.",
+                                      icon: "success",
+                                      button: "Ok",
+                                    });
+                                });
+                            </script>';
+                        } else {
+                            echo '
+                            <script type="text/javascript">
+                                jQuery(function validation() {
+                                    swal({
+                                      title: "Error!",
+                                      text: "The product is not updated.",
+                                      icon: "error",
+                                      button: "Ok",
+                                    });
+                                });
+                            </script>'; 
+                        }
+                    }
+               } 
+            }   
+        }else {
+            $error =  '
+            <script type="text/javascript">
+                jQuery(function validation() {
+                    swal({
+                      title: "Warning!!!",
+                      text: "only jpg, jpeg, png and gif can be upload",
+                      icon: "warning",
+                      button: "Ok",
+                    });
+                });
+            </script>'; 
+            echo $error; 
+        }
+        
     } else {
         $update = $pdo->prepare("update tbl_product set pname=:pname, pcategory=:pcategory, purchaseprice=:purchaseprice, saleprice=:saleprice, pstock=:pstock, pdescription=:pdescription, pimage=:pimage where pid = $id"); 
         $update->bindParam(':pname', $productname_txt); 
@@ -72,6 +152,20 @@ if(isset($_POST['btnupdate'])) {
     
 }
 
+$select = $pdo->prepare("select * from tbl_product where pid = $id"); 
+$select->execute(); 
+
+$row = $select->fetch(PDO::FETCH_ASSOC); 
+//values from database 
+$id_db = $row['pid']; 
+$productname_db = $row['pname'];
+$category_db = $row['pcategory'];
+$purchaseprice_db = $row['purchaseprice'];
+$saleprice_db = $row['saleprice'];
+$stock_db = $row['pstock'];
+$description_db = $row['pdescription'];
+$productimage_db = $row['pimage'];
+
 ?>
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
@@ -95,7 +189,7 @@ if(isset($_POST['btnupdate'])) {
         -------------------------->
         <div class="box box-warning">
             <div class="box-header with-border">
-              <h3 class="box-title">Product Update Form</h3>
+              <h3 class="box-title"><a href="productlist.php" class="btn btn-primary" role="button">Back to Product List</a></h3>
             </div>
             <!-- /.box-header -->
             <!-- form start -->
