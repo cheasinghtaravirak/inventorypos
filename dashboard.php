d2659c9df3c55f8b8d701093b0f77f24e1dec41d
Singhtaravirak Chea <singhtaravirak@gmail.com>
2019-10-29 09:38:40 +0700

    Start implementing admin dashboard in dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -6,6 +6,12 @@ if($_SESSION['useremail']=="") {
     header('location:index.php');
 }
 
+$select = $pdo->prepare("select sum(total) as t, count(invoice_id) as inv from tbl_invoice");
+$select->execute(); 
+$row = $select->fetch(PDO::FETCH_OBJ); 
+
+$total_order = $row->inv; 
+$net_total = $row->t; 
 
 include_once'header.php';
 ?>
@@ -29,7 +35,85 @@ include_once'header.php';
       <!--------------------------
         | Your Page Content Here |
         -------------------------->
+        <div class="box-body">
+          <!-- Small boxes (Stat box) -->
+          <div class="row">
+            <div class="col-lg-3 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-aqua">
+                <div class="inner">
+                  <h3><?php echo $total_order; ?></h3>
 
+                  <p>Total Orders</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-bag"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-green">
+                <div class="inner">
+                  <h3><?php echo "$".number_format($net_total, 2); ?><sup style="font-size: 20px"></sup></h3>
+
+                  <p>Total Revenue</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+            
+            <?php
+                $select = $pdo->prepare("select count(pname) as p from tbl_product");
+                $select->execute(); 
+                $row = $select->fetch(PDO::FETCH_OBJ); 
+                $total_product = $row->p; 
+            ?>
+            <!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-yellow">
+                <div class="inner">
+                  <h3><?php echo $total_product; ?></h3>
+
+                  <p>Total Products</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-person-add"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+            <?php
+                $select = $pdo->prepare("select count(category) as cate from tbl_category");
+                $select->execute(); 
+                $row = $select->fetch(PDO::FETCH_OBJ); 
+                $total_category = $row->cate; 
+            ?>
+            <!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-red">
+                <div class="inner">
+                  <h3><?php echo $total_category; ?></h3>
+
+                  <p>Total Categories</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+            <!-- ./col -->
+          </div>
+          <!-- /.row -->
+        </div>
     </section>
     <!-- /.content -->
   </div>
